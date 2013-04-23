d9a6dca7dedee4a8af94597652ef72a8add18b2a
Vincent Fantini <vincent.fantini@gmail.com>
2013-04-22 20:07:30 -0400

    Improved the functional tests for the Store controller.

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,7 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.navigation a', minimum: 5
+    assert_select '.navigation_this', maximum: 1
+    assert_select '#main .entry', 3
+    assert_select 'h3', '3-ft. White Nylon Whip'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
-
 end
